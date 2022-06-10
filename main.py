3d48decf4ffbfbb3f7c67de9c160041d62a55716
Zachary Hoppock <61926087+zhoppock@users.noreply.github.com>
2022-06-10 19:29:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+dates = 45
+
+while dates < 50:
+  print("Date number ", dates)
+  dates = dates+1
+
+print("whoo she remembers me! ")
+
+print("\n")
+
+for number in range(5):
+  print("Thank you")
+
+print("\n")
+
+languages = ['C++', 'Python', 'C', 'Java', 'cobol']
+
+for index in range(len(languages)):
+  print('Current language:', languages[index])
